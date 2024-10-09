b19cdf5b5662e588b026b78d16cd8e099f7a54c8
gamastronger <refasetyagama@gmail.com>
2024-10-09 19:57:28 +0700

    website gayatama

--- src/Component/Layanan/Ig.jsx
+++ src/Component/Layanan/Ig.jsx
@@ -1,17 +1,17 @@
 const Ig = () => {
     return (
-      <div className="relative bg-[#0F172A] flex flex-col 2xl:flex-row items-center justify-center text-white px-4 -mt-16 md:pt-16 2xl:-mt-56 2xl:px-24 mb-20 2xl:pb-10 overflow-hidden">
+      <div className="relative bg-[#0F172A] flex flex-col xl:flex-row items-center justify-center text-white px-4 -mt-16 md:pt-16 xl:-mt-56 xl:px-24 mb-20 xl:pb-10 overflow-hidden">
         {/* Right Section (Cards) */}
-        <div className="2xl:flex 2xl:flex-row 2xl:space-x-8 w-full flex justify-center pt-20 pb-14 md:pb-12 md:pt-20 2xl:px-0 relative z-10">
+        <div className="xl:flex xl:flex-row xl:space-x-8 w-full flex justify-center pt-20 pb-14 md:pb-12 md:pt-20 xl:px-0 relative z-10">
           {/* Import Method Card with blue blur background */}
-          <div className="absolute 2xl:static mt-24 md:mt-32 2xl:mt-44 w-48 h-32 md:w-[350px] md:h-[250px] 2xl:w-[320px] 2xl:h-[200px] bg-slate-300 rounded-lg shadow-lg opacity-70 transform rotate-[-5deg] hover:rotate-0 hover:scale-105 transition-all duration-500 ease-in-out hover:shadow-2xl">
+          <div className="absolute xl:static mt-24 md:mt-32 xl:mt-44 w-48 h-32 md:w-[350px] md:h-[250px] xl:w-[320px] xl:h-[200px] bg-slate-300 rounded-lg shadow-lg opacity-70 transform rotate-[-5deg] hover:rotate-0 hover:scale-105 transition-all duration-500 ease-in-out hover:shadow-2xl">
             <div className="absolute inset-0 -z-10 bg-blue-400 opacity-60 blur-xl rounded-lg"></div> {/* Blue Blur */}
             <div className="h-full flex items-center justify-center text-lg md:text-xl font-semibold text-black">
               Import Method
             </div>
           </div>
           {/* Actions Card with blue blur background */}
-          <div className="relative 2xl:mt-24 2xl:static w-48 h-32 md:w-[350px] md:h-[250px] 2xl:w-[320px] 2xl:h-[200px] bg-slate-300 rounded-lg shadow-lg opacity-90 transform rotate-[5deg] hover:rotate-0 hover:scale-110 transition-all duration-500 ease-in-out hover:shadow-2xl">
+          <div className="relative xl:mt-24 xl:static w-48 h-32 md:w-[350px] md:h-[250px] xl:w-[320px] xl:h-[200px] bg-slate-300 rounded-lg shadow-lg opacity-90 transform rotate-[5deg] hover:rotate-0 hover:scale-110 transition-all duration-500 ease-in-out hover:shadow-2xl">
             <div className="absolute inset-0 -z-10 bg-blue-400 opacity-60 blur-xl rounded-lg"></div> {/* Blue Blur */}
             <div className="h-full flex items-center justify-center text-lg md:text-xl font-semibold text-black">
               Actions
@@ -20,11 +20,11 @@ const Ig = () => {
         </div>
   
         {/* Text and Button Section */}
-        <div className="md:text-left mt-20 md:mt-36 mx-2 md:mx-28 2xl:mx-0 text-[14px] md:text-xl 2xl:text-lg text-white relative z-10">
-          <h2 className="text-3xl md:text-4xl 2xl:text-5xl 2xl:font-semibold transition-transform duration-500 hover:scale-105 hover:text-blue-400">
+        <div className="md:text-left mt-20 md:mt-36 mx-2 md:mx-28 xl:mx-0 text-[14px] md:text-xl xl:text-lg text-white relative z-10">
+          <h2 className="text-3xl md:text-4xl xl:text-5xl xl:font-semibold transition-transform duration-500 hover:scale-105 hover:text-blue-400">
             Save time and money with our powerful tools
           </h2>
-          <p className="text-gray-400 2xl:text-white mt-4 2xl:pt-4 2xl:pb-3">
+          <p className="text-gray-400 xl:text-white mt-4 xl:pt-4 xl:pb-3">
             Create powerful websites effortlessly, whether youre working solo or
             collaborating with a team.
           </p>
--- src/Component/Layanan/Judul.jsx
+++ src/Component/Layanan/Judul.jsx
@@ -11,27 +11,27 @@ const Judul = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="mx-auto text-center px-6 2xl:px-8 md:mt-6 2xl:mt-16 md:mx-5">
-        <p className="text-4xl md:text-5xl 2xl:text-[65px] font-semibold tracking-tight">
+      <div className="mx-auto text-center px-6 xl:px-8 md:mt-6 xl:mt-16 md:mx-5">
+        <p className="text-4xl md:text-5xl xl:text-[65px] font-semibold tracking-tight">
           Layanan Grafistix
         </p>
-        <p className="pt-4 2xl:pt-8 text-[15px] md:text-lg">
+        <p className="pt-4 xl:pt-8 text-[15px] md:text-lg">
           Kami adalah pilihan tepat untuk Anda yang membutuhkan desain grafis
           profesional, kreatif, dan berkualitas.
         </p>
-        <div className="pt-8 md:pt-12 2xl:pt-16 flex justify-center">
+        <div className="pt-8 md:pt-12 xl:pt-16 flex justify-center">
           <img className="" src={pict} alt="Layanan Grafistix" />
         </div>
       </div>
-      <div className="mx-auto px-6 2xl:px-8 mt-8 md:pt-10 2xl:pt-12 text-[15px] text-white md:text-lg md:text-center md:mx-5">
-        <p className="text-2xl md:text-4xl 2xl:text-[45px] font-medium tracking-tight">
+      <div className="mx-auto px-6 xl:px-8 mt-8 md:pt-10 xl:pt-12 text-[15px] text-white md:text-lg md:text-center md:mx-5">
+        <p className="text-xl md:text-4xl xl:text-[45px] font-medium tracking-tight">
           Selalu Siap Melayani Anda
         </p>
-        <p className="pt-4 2xl:pt-7">
+        <p className="pt-4 xl:pt-7">
           Wujudkan ide-ide Anda menjadi kenyataan dengan sentuhan desain
           profesional, bekerja sama secara fleksibel dan mudah.
         </p>
-        <div className="pt-4 2xl:pt-7">
+        <div className="pt-4 xl:pt-7">
           <button className="border border-gray-700 px-6 py-2 rounded-lg hover:bg-transparent hover:border-white transition text-align:center">
             Selengkapnya
           </button>
